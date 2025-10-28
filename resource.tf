077f4cb65fb7f9930bcdfd6614b8ad62c866317e
jagdishmodi <48287274+jagdishmodi@users.noreply.github.com>
2025-10-28 14:36:30 +0530

    Create resource.tf

---
+++ resource.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "example" {
+  ami           = "ami-07860a2d7eb515d9a" # Amazon Linux 2 AMI (update as needed)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
