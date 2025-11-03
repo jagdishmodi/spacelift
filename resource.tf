59923a3bc28b5f555e80af5f3124be3358248266
jagdishmodi <48287274+jagdishmodi@users.noreply.github.com>
2025-11-03 14:47:46 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   ami           = "ami-07860a2d7eb515d9a" # Amazon Linux 2 AMI (update as needed)
-  instance_type = "t2.large"
+  instance_type = "t2.xlarge"
 
   tags = {
     Name = "Terraform-EC2"
